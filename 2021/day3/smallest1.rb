15eb4482037838693dd8208757d8405b5f694dae
clado <2933589+clado@users.noreply.github.com>
2021-12-03 11:28:50 -0800

    [2021] day 3: MORE CURSED
    RUBBER DUCKED WITH A FRIEND AND IMPROVED THE TERROR LEVEL IN THIS
    SOLUTION
    
    having 0..11 on the outer level rather than lines is
    _significantly_ less performant and it makes me itch, but I guess
    that's why it's part of the cursed solution. :upside_down_smile:

--- 2021/day3/smallest1.rb
+++ 2021/day3/smallest1.rb
@@ -2,8 +2,7 @@
 
 lines = IO.readlines('input.txt', chomp: true)
 
-ones = [0] * 12
-lines.each { |number| number.split('').each_index { |bit| ones[bit] += number[bit].to_i } }
+ones = (0..11).map { |bit| lines.sum { |number| number[bit].to_i } }
 
 gamma = ones.map { |bit|  bit > lines.size / 2 ? '1' : '0' }.join
 epsilon = ones.map { |bit| bit > lines.size / 2 ? '0' : '1' }.join
