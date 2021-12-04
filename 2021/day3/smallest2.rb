0f06dab17a3808415092a3fcf553a87de1304129
clado <2933589+clado@users.noreply.github.com>
2021-12-03 17:56:03 -0800

    [2021] day 3: more cursed
    I copied a friend's recursive solution to make this even worse.

--- 2021/day3/smallest2.rb
+++ 2021/day3/smallest2.rb
@@ -1,16 +1,11 @@
 # warning: cursed.
 
-def solve(set, func)
-  bit = 0
-  while set.size > 1 do
-    most_common = set.sum { |num| num[bit].to_i } >= set.size / 2.0 ? '1' : '0'
-    set.send(func) { |n| n[bit] == most_common }
-    bit += 1
-  end
-  set
+def solve(set, func, bit = 0)
+  return set.first if set.size == 1
+  most_common = set.sum { |num| num[bit].to_i } >= set.size / 2.0 ? '1' : '0'
+  solve(set.send(func) { |n| n[bit] == most_common }, func, bit + 1)
 end
 
 oxygen_set = IO.readlines('input.txt', chomp: true)
-co2_set = oxygen_set.dup
 
-puts(solve(oxygen_set, :select!).first.to_i(2) * solve(co2_set, :reject!).first.to_i(2))
+puts(solve(oxygen_set, :select).to_i(2) * solve(oxygen_set.dup, :reject).to_i(2))
